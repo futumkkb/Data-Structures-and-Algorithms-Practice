1c79b232bf40a6864625e09ce6c57edb701b86d0
André Rodrigues <mrnexusrider@hotmail.com>
2023-12-20 19:11:49 -0300

    Inverting a Binary Tree

---
+++ src/BinaryTree/InvertBinaryTree.java
@@ -0,0 +1,33 @@
+package BinaryTree;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+      }
+  }
+
+class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+
+        var left = root.left;
+        var right = root.right;
+
+        root.right = left;
+        root.left = right;
+
+        invertTree(left);
+        invertTree(right);
+
+        return root;
+
+    }
+}
